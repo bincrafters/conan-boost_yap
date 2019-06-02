8c74620e62c106270ff83ea4ee0f3e44569187db
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:09 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,16 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
 
-class BoostConvertConan(base.BoostBaseConan):
+
+class BoostYapConan(base.BoostBaseConan):
     name = "boost_yap"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_yap"
-    lib_short_names = ["yap"]
-    header_only_libs = ["yap"]
-    b2_requires = [
-        "boost_hana",
-        "boost_preprocessor",
-        "boost_type_index"
-    ]
+    version = "1.70.0"
